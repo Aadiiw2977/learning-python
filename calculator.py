a558d67ec7fc01e307e0a0a5ae1cc9903ea1adad
AADIIW <muhammadadeel8083@gmail.com>
2026-07-09 17:15:40 +0500

    Added Calculator Project

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("━━━━━━━━━━━━━━━━━━━━")
+print("🧮 SIMPLE CALCULATOR")
+print("━━━━━━━━━━━━━━━━━━━━")
+
+a = float(input("First Number: "))
+b = float(input("Second Number: "))
+
+print("Addition      =", a + b)
+print("Subtraction   =", a - b)
+print("Multiplication=", a * b)
+print("Division      =", a / b)
