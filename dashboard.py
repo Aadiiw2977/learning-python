7412f93e9384216465aced83dc40d271cf288489
AADIIW <muhammadadeel8083@gmail.com>
2026-07-09 17:19:44 +0500

    Updated VIP Dashboard

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+import platform
+from datetime import datetime
+
+name = "AADIIW"
+
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print("🔥 𝐀𝐀𝐃𝐈𝐈𝐖 𝐃𝐄𝐕𝐈𝐋 🔥")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print(f"👤 Name    : {name}")
+print(f"🎂 Age     : 20")
+print(f"💻 System  : {platform.system()}")
+print(f"📱 Device  : {platform.machine()}")
+print(f"🐍 Python  : {platform.python_version()}")
+print(f"🕒 Time    : {datetime.now().strftime('%H:%M:%S')}")
+print("🟢 Status  : ONLINE")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
